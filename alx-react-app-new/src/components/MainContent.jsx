b1becc0e79d4bd781ab4142fafa3f335d5b0bc1c
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2025-09-07 23:33:51 +0100

    Initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'justify' }}>
+      <p>
+        Welcome to the list of my favorite cities! Here, you will find a curated list of places I love to visit.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
